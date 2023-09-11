e4c600a10eeb6e0ce516420aae2cf45826995c88
Shubhamthora <Shubhamthorat2001@gmail.com>
2023-09-11 23:43:31 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask  , jsonify
+from flask import render_template
+import openai
+api= "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"
+app = Flask(__name__,template_folder='templates')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generateimages/<prompt>')
+def generate(prompt):
+  print("prompt:", prompt)
+  response = openai.Image.create(prompt=prompt, n=5, size="256x256") 
+  print(response)
+  return jsonify(response)
+
+ 
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
